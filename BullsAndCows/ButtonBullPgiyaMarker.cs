5ea407b80867226a0a60fe880eaa5a4a580f82e4
romerachlin1 <romerachlin1@gmail.com>
2025-08-31 13:51:17 +0300

    Add files via upload

---
+++ BullsAndCows/ButtonBullPgiyaMarker.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ex05
+{
+    public class ButtonBullPgiyaMarker : Button
+    {
+        public ButtonBullPgiyaMarker()
+        {
+            Size = new Size(10, 10);
+            Enabled = false;
+        }
+
+        public void ChangeColorToBull()
+        {
+            BackColor = Color.Black;
+        }
+
+        public void ChangeColorToPgiya()
+        {
+            BackColor = Color.Yellow;
+        }
+    }
+}
---
+++ BullsAndCows/GuessValidation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+namespace Ex05
+{
+    public class GuessValidation
+    {
+        private int m_Bull = 0;
+        private int m_Pgiya = 0;
+        private readonly List<Color> r_ComputerColors = new List<Color>();
+        private readonly List<Color> r_UserGuess = new List<Color>();
+        private bool m_DidPlayerWin = false;
+
+        public int Bull
+        {
+            get { return m_Bull; }
+        }
+
+        public int Pgiya
+        {
+            get { return m_Pgiya; }
+        }
+
+        public bool DidPlayerWin
+        {
+            get { return m_DidPlayerWin; }
+        }
+
+        public GuessValidation(List<Color> i_UserGuess, List<Color> i_ComputerColors)
+        {
+            r_UserGuess = i_UserGuess;
+            r_ComputerColors = i_ComputerColors;
+            compareUserGuessToComputer();
+            CheckPlayerWon();
+        }
+
+        public void compareUserGuessToComputer()
+        {
+            for (int i = 0; i < r_UserGuess.Count; i++)
+            {
+                if (r_UserGuess[i] == r_ComputerColors[i])
+                {
+                    m_Bull++;
+                }
+
+                for (int j = 0; j < r_UserGuess.Count; j++)
+                {
+                    if (j != i)
+                    {
+                        if (r_UserGuess[i] == r_ComputerColors[j])
+                        {
+                            m_Pgiya++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void CheckPlayerWon()
+        {
+            if (m_Bull == 4)
+            {
+                m_DidPlayerWin = true;
+            }
+        }
+    }
+}
